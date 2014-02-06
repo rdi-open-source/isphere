fa2062e24eacdb928e76c6e6bfe2bcd32e2a56bb
Frank Hildebrandt <fhildebrandt@taskforce-it.de>
2014-02-06 16:28:06 +0000

    Source File Search / Message File Search

--- rse1/iSphereRSEPlugin/src/de/taskforce/isphere/rse/ISphereRSEPlugin.java
+++ rse1/iSphereRSEPlugin/src/de/taskforce/isphere/rse/ISphereRSEPlugin.java
@@ -23,6 +23,8 @@ import org.osgi.framework.BundleContext;
 
 import de.taskforce.isphere.ISpherePlugin;
 import de.taskforce.isphere.rse.internal.Editor;
+import de.taskforce.isphere.rse.internal.MessageFileSearchObjectFilterCreator;
+import de.taskforce.isphere.rse.internal.SourceFileSearchMemberFilterCreator;
 import de.taskforce.isphere.rse.spooledfiles.SpooledFileAdapterFactory;
 import de.taskforce.isphere.rse.spooledfiles.SpooledFileResource;
 
@@ -40,6 +42,8 @@ public class ISphereRSEPlugin extends AbstractUIPlugin {
 		super.start(context);
 		installURL = context.getBundle().getEntry("/");
 		ISpherePlugin.setEditor(new Editor());
+		ISpherePlugin.setSourceFileSearchMemberFilterCreator(new SourceFileSearchMemberFilterCreator());
+		ISpherePlugin.setMessageFileSearchObjectFilterCreator(new MessageFileSearchObjectFilterCreator());
 		setupAdapters();	
 	}
 
---
+++ rse1/iSphereRSEPlugin/src/de/taskforce/isphere/rse/internal/RSEHelper.java
@@ -0,0 +1,107 @@
+package de.taskforce.isphere.rse.internal;
+
+import java.util.Vector;
+
+import com.ibm.etools.iseries.comm.filters.ISeriesLibraryFilterString;
+import com.ibm.etools.iseries.comm.filters.ISeriesMemberFilterString;
+import com.ibm.etools.iseries.comm.filters.ISeriesObjectFilterString;
+import com.ibm.etools.iseries.core.api.ISeriesConnection;
+import com.ibm.etools.systems.filters.SystemFilter;
+import com.ibm.etools.systems.filters.SystemFilterPool;
+import com.ibm.etools.systems.filters.SystemFilterPoolManager;
+import com.ibm.etools.systems.subsystems.SubSystem;
+
+public class RSEHelper {
+
+	public static SystemFilter createMemberFilter(
+			ISeriesConnection connection, 
+			String filterName,
+			ISeriesMemberFilterString[] filterStrings) {
+
+        Vector<String> _filterStrings = new Vector<String>();
+        for (int idx = 0; idx < filterStrings.length; idx++) {
+            _filterStrings.add(filterStrings[idx].toString());
+        }
+
+        return createFilter(connection, "Member", filterName, _filterStrings);
+		
+	}
+
+	public static SystemFilter createObjectFilter(
+			ISeriesConnection connection, 
+			String filterName,
+			ISeriesObjectFilterString[] filterStrings) {
+
+        Vector<String> _filterStrings = new Vector<String>();
+        for (int idx = 0; idx < filterStrings.length; idx++) {
+            _filterStrings.add(filterStrings[idx].toString());
+        }
+
+        return createFilter(connection, "Object", filterName, _filterStrings);
+		
+	}
+
+	public static SystemFilter createLibraryFilter(
+			ISeriesConnection connection, 
+			String filterName,
+			ISeriesLibraryFilterString[] filterStrings) {
+
+        Vector<String> _filterStrings = new Vector<String>();
+        for (int idx = 0; idx < filterStrings.length; idx++) {
+            _filterStrings.add(filterStrings[idx].toString());
+        }
+
+        return createFilter(connection, "Library", filterName, _filterStrings);
+		
+	}
+	
+	public static SystemFilter createFilter(
+			ISeriesConnection connection, 
+			String filterType, 
+			String filterName, 
+			Vector filterStrings) {
+		
+        SubSystem subsystem = connection.getISeriesFileSubSystem();
+        
+        if (subsystem != null) {
+        	
+            SystemFilterPool pools[] = subsystem.getFilterPoolReferenceManager().getReferencedSystemFilterPools();
+            
+            if (pools != null) {
+            	
+            	SystemFilterPool defaultPool = null;
+                for (int idx = 0; idx < pools.length; idx++) {
+                    if (pools[idx].isDefault()) {
+                        defaultPool = pools[idx];
+                        break;
+                    }
+                }
+            	
+                if (defaultPool != null) {
+                    
+                    Vector filterNames = defaultPool.getSystemFilterNames();
+                    for (int idx = 0; idx < filterNames.size(); idx++) {
+                    	if (((String)filterNames.get(idx)).equals(filterName)) {
+                    		return null;
+                    	}
+                    }
+                	
+                    SystemFilterPoolManager dftPoolMgr = subsystem.getFilterPoolReferenceManager().getDefaultSystemFilterPoolManager();
+                    
+                    try {
+                        return dftPoolMgr.createSystemFilter(defaultPool, filterName, filterStrings, filterType);
+                    }
+                    catch(Exception e) {
+                    }
+                	
+                }
+                
+            }
+        	
+        }
+        
+        return null;
+        
+	}
+	
+}
