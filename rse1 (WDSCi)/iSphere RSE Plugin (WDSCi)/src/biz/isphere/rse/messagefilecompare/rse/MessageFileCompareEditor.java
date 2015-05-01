78e8720d625f4b1112f3453a73d1b74c6277f100
tools400 <thomas.raddatz@tools400.de>
2015-05-01 13:04:14 +0000

    Centered compare status image.

--- rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/messagefilecompare/rse/MessageFileCompareEditor.java
+++ rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/messagefilecompare/rse/MessageFileCompareEditor.java
@@ -8,6 +8,9 @@
 
 package biz.isphere.rse.messagefilecompare.rse;
 
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+
 import biz.isphere.core.internal.RemoteObject;
 import biz.isphere.core.messagefilecompare.rse.AbstractMessageFileCompareEditor;
 import biz.isphere.rse.internal.RSESelectObjectDialog;
@@ -30,6 +33,25 @@ public class MessageFileCompareEditor extends AbstractMessageFileCompareEditor {
         }
 
         return dialog.getRemoteObject();
+    }
+
+    @Override
+    protected LabelProvider getTableLabelProvider(TableViewer tableViewer, int columnIndex) {
+        return new TableLabelProvider(tableViewer, columnIndex);
+    }
+
+    /**
+     * Class the provides the content for the cells of the table.
+     */
+    private class TableLabelProvider extends AbstractTableLabelProvider {
+
+        public TableLabelProvider(TableViewer tableViewer, int columnIndex) {
+            super(tableViewer, columnIndex);
+        }
+
+        protected boolean useCompareStatusImagePainter() {
+            return false;
+        }
     }
 
 }
