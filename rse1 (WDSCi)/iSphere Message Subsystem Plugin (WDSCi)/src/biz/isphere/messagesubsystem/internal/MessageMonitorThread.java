610f3c3f26cc245fbc4b7f67a4d2d32910ec232c
tools400 <thomas.raddatz@tools400.de>
2015-07-09 08:34:08 +0000

    Added missing copyright header.

--- rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/internal/MessageMonitorThread.java
+++ rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/internal/MessageMonitorThread.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 iSphere Project Owners
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     iSphere Project Owners - Maintenance and enhancements
+ *******************************************************************************/
+
 package biz.isphere.messagesubsystem.internal;
 
 import org.eclipse.jface.dialogs.MessageDialog;
