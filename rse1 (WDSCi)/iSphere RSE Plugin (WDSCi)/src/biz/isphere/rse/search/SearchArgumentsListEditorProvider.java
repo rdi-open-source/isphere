044bbeed56ad7f7bf60716dd4282868b9610fcbb
tools400 <thomas.raddatz@tools400.de>
2015-07-26 16:07:17 +0000

    Changed STRPREPRC to also write the event file for pre- and post commands.

--- rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/search/SearchArgumentsListEditorProvider.java
+++ rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/search/SearchArgumentsListEditorProvider.java
@@ -13,8 +13,8 @@ import biz.isphere.core.search.SearchOptions;
 
 public class SearchArgumentsListEditorProvider implements ISearchArgumentsListEditorProvider {
 
-    public biz.isphere.core.search.SearchArgumentsListEditor getListEditor() {
-        return new SearchArgumentsListEditor(SearchOptions.ARGUMENTS_SIZE, true);
+    public biz.isphere.core.search.SearchArgumentsListEditor getListEditor(boolean regularExpressionsOption) {
+        return new SearchArgumentsListEditor(SearchOptions.ARGUMENTS_SIZE, regularExpressionsOption);
     }
 
 }
