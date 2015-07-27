75b067e36e3499eb03fb82df44d96df995c75018
tools400 <thomas.raddatz@tools400.de>
2015-07-27 11:57:33 +0000

    Changed error handling of invalid email attributes and send email errors.

--- rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/internal/MessageHandler.java
+++ rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/internal/MessageHandler.java
@@ -34,20 +34,14 @@ public class MessageHandler implements IMessageHandler {
         if (!monitoringAttributes.isMonitoring()) {
             return;
         }
-        
+
         final QueuedMessage msg = message;
         Display.getDefault().syncExec(new Runnable() {
             public void run() {
 
                 MonitoringAttributes monitoringAttributes = new MonitoringAttributes(queuedMessageSubSystem);
 
-                String handling;
-
-                if (msg.getType() == QueuedMessage.INQUIRY) {
-                    handling = monitoringAttributes.getInqueryMessageNotificationType();
-                } else {
-                    handling = monitoringAttributes.getInformationalMessageNotificationType();
-                }
+                String handling = getMessageHandling(monitoringAttributes);
 
                 if (MonitoringAttributes.NOTIFICATION_TYPE_BEEP.equals(handling)) {
                     Display.getDefault().beep();
@@ -58,32 +52,40 @@ public class MessageHandler implements IMessageHandler {
                     if (!monitoringAttributes.isValid()) {
                         if (MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.ISeries_Message_Email_Error,
                             Messages.Email_Notification_Error_Message)) {
-                            QueuedMessageDialog dialog = new QueuedMessageDialog(Display.getDefault().getActiveShell(), msg, false);
+                            if (msg.getType() == QueuedMessage.INQUIRY) {
+                                monitoringAttributes.setInqueryMessageNotificationType(MonitoringAttributes.NOTIFICATION_TYPE_DIALOG);
+                            } else {
+                                monitoringAttributes.setInformationalMessageNotificationType(MonitoringAttributes.NOTIFICATION_TYPE_DIALOG);
+                            }
+                            handling = getMessageHandling(monitoringAttributes);
+                        }
+                    } else {
+
+                        MessageQueueMailMessenger messenger = new MessageQueueMailMessenger();
+                        messenger.setRecipients(new String[] { monitoringAttributes.getEmail() });
+                        messenger.setMailFrom(monitoringAttributes.getFrom());
+                        messenger.setHost(monitoringAttributes.getHost());
+                        messenger.setPort(monitoringAttributes.getPort());
+
+                        try {
+                            messenger.sendMail(msg);
+                        } catch (Exception e) {
+
+                            String errorMessage = e.getMessage();
+                            if (errorMessage == null) {
+                                errorMessage = e.toString();
+                            }
+
+                            errorMessage = errorMessage + Messages.Email_Notification_Properties_Error_message;
                             Display.getDefault().beep();
-                            dialog.open();
-                            return;
-                        }
-                    }
-
-                    MessageQueueMailMessenger messenger = new MessageQueueMailMessenger();
-                    messenger.setRecipients(new String[] { monitoringAttributes.getEmail() });
-                    messenger.setMailFrom(monitoringAttributes.getFrom());
-                    messenger.setHost(monitoringAttributes.getHost());
-                    messenger.setPort(monitoringAttributes.getPort());
-
-                    try {
-                        messenger.sendMail(msg);
-                    } catch (Exception e) {
-
-                        String errorMessage = e.getMessage();
-                        if (errorMessage == null) {
-                            errorMessage = e.toString();
-                        }
-
-                        errorMessage = errorMessage + Messages.Email_Notification_Properties_Error_message;
-                        Display.getDefault().beep();
-                        if (MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.ISeries_Message_Email_Error, errorMessage)) {
-                            handling = MonitoringAttributes.NOTIFICATION_TYPE_DIALOG;
+                            if (MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.ISeries_Message_Email_Error, errorMessage)) {
+                                if (msg.getType() == QueuedMessage.INQUIRY) {
+                                    monitoringAttributes.setInqueryMessageNotificationType(MonitoringAttributes.NOTIFICATION_TYPE_DIALOG);
+                                } else {
+                                    monitoringAttributes.setInformationalMessageNotificationType(MonitoringAttributes.NOTIFICATION_TYPE_DIALOG);
+                                }
+                                handling = getMessageHandling(monitoringAttributes);
+                            }
                         }
                     }
                 }
@@ -97,6 +99,14 @@ public class MessageHandler implements IMessageHandler {
 
                 if (!MonitoringAttributes.NOTIFICATION_TYPE_BEEP.equals(handling)) {
                     removeInformationalMessage(msg, monitoringAttributes);
+                }
+            }
+
+            private String getMessageHandling(MonitoringAttributes monitoringAttributes) {
+                if (msg.getType() == QueuedMessage.INQUIRY) {
+                    return monitoringAttributes.getInqueryMessageNotificationType();
+                } else {
+                    return monitoringAttributes.getInformationalMessageNotificationType();
                 }
             }
 
