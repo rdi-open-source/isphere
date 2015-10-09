eff12dcee4e6d53d724969ffec794423c3e21b25
tools400 <thomas.raddatz@tools400.de>
2015-10-09 18:55:07 +0000

    Moved program code that is shared between RDi and WDSCi to new plug-in 'iSphere Message Subsystem Plugin'.

--- rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/rse/Messages.java
+++ rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/rse/Messages.java
@@ -26,47 +26,7 @@ public class Messages extends NLS {
 
     public static String Change_Message_Filter;
 
-    public static String Message_queue_colon;
-
-    public static String Library_colon;
-
-    public static String From_user_colon;
-
-    public static String Message_ID_colon;
-
-    public static String Severity_threshold_colon;
-
-    public static String From_job_colon;
-
-    public static String From_job_number_colon;
-
-    public static String From_program_colon;
-
-    public static String Message_text_contains_colon;
-
-    public static String Message_type_colon;
-
     public static String Message_queue_and_library_must_be_specified;
-
-    public static String Delete_Message_Error;
-
-    public static String From;
-
-    public static String Message_ID;
-
-    public static String Severity;
-
-    public static String Message_type;
-
-    public static String Date_sent;
-
-    public static String From_job;
-
-    public static String From_job_number;
-
-    public static String From_program;
-
-    public static String Reply_status;
 
     static {
         // initialize resource bundle
--- rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/rse/internal/QueuedMessageResourceAdapter.java
+++ rse1 (WDSCi)/iSphere Message Subsystem Plugin (WDSCi)/src/biz/isphere/messagesubsystem/rse/internal/QueuedMessageResourceAdapter.java
@@ -11,19 +11,14 @@
  *******************************************************************************/
 package biz.isphere.messagesubsystem.rse.internal;
 
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
-import org.eclipse.ui.views.properties.PropertyDescriptor;
 
+import biz.isphere.messagesubsystem.rse.IQueuedMessageResource;
 import biz.isphere.messagesubsystem.rse.ISphereMessageSubsystemRSEPlugin;
-import biz.isphere.messagesubsystem.rse.Messages;
-import biz.isphere.messagesubsystem.rse.QueuedMessageDialog;
-import biz.isphere.messagesubsystem.rse.QueuedMessageHelper;
-import biz.isphere.messagesubsystem.rse.ReceivedMessage;
+import biz.isphere.messagesubsystem.rse.QueuedMessageResourceAdapterDelegate;
 
 import com.ibm.as400.access.QueuedMessage;
 import com.ibm.etools.systems.core.ui.SystemMenuManager;
@@ -32,21 +27,12 @@ import com.ibm.etools.systems.core.ui.view.ISystemRemoteElementAdapter;
 
 public class QueuedMessageResourceAdapter extends AbstractSystemViewAdapter implements ISystemRemoteElementAdapter {
 
-    private static final String KEY_REPLY_STS = "replySts"; //$NON-NLS-1$
-    private static final String KEY_PGM = "pgm"; //$NON-NLS-1$
-    private static final String KEY_JOBNBR = "jobnbr"; //$NON-NLS-1$
-    private static final String KEY_JOB = "job"; //$NON-NLS-1$
-    private static final String KEY_DATE = "date"; //$NON-NLS-1$
-    private static final String KEY_TYPE = "type"; //$NON-NLS-1$
-    private static final String KEY_SEV = "sev"; //$NON-NLS-1$
-    private static final String KEY_MSGID = "msgid"; //$NON-NLS-1$
-    private static final String KEY_FROM = "from"; //$NON-NLS-1$
-
-    private static final String QUEUED_MESSAGE = "Queued message"; //$NON-NLS-1$
-    private static final String QUEUED_MESSAGE_RESOURCE = "Queued message resource"; //$NON-NLS-1$
+    private QueuedMessageResourceAdapterDelegate delegate;
 
     public QueuedMessageResourceAdapter() {
         super();
+
+        delegate = new QueuedMessageResourceAdapterDelegate();
     }
 
     @Override
@@ -65,49 +51,42 @@ public class QueuedMessageResourceAdapter extends AbstractSystemViewAdapter impl
 
     @Override
     public boolean handleDoubleClick(Object object) {
-        if (object instanceof QueuedMessageResource) {
-            QueuedMessageResource queuedMessageResource = (QueuedMessageResource)object;
-            QueuedMessage queuedMessage = queuedMessageResource.getQueuedMessage();
-            QueuedMessageDialog dialog = new QueuedMessageDialog(Display.getCurrent().getActiveShell(), new ReceivedMessage(queuedMessage));
-            dialog.open();
-        }
-        return false;
+        return delegate.handleDoubleClick(object);
     }
 
     @Override
     public String getText(Object element) {
-        return ((QueuedMessageResource)element).getQueuedMessage().getText();
+        return delegate.getText(element);
     }
 
     @Override
     public String getAbsoluteName(Object object) {
-        QueuedMessageResource queuedMessageResource = (QueuedMessageResource)object;
-        return QUEUED_MESSAGE + queuedMessageResource.getQueuedMessage().getKey();
+        return delegate.getAbsoluteName(object);
     }
 
     @Override
     public String getType(Object element) {
-        return QUEUED_MESSAGE_RESOURCE;
+        return delegate.getType();
     }
 
     @Override
     public Object getParent(Object element) {
-        return null;
+        return delegate.getParent();
     }
 
     @Override
     public boolean hasChildren(Object element) {
-        return false;
+        return delegate.hasChildren();
     }
 
     @Override
     public boolean showDelete(Object element) {
-        return true;
+        return delegate.showDelete();
     }
 
     @Override
     public boolean canDelete(Object element) {
-        return true;
+        return delegate.canDelete();
     }
 
     @Override
@@ -116,104 +95,29 @@ public class QueuedMessageResourceAdapter extends AbstractSystemViewAdapter impl
         QueuedMessageResource queuedMessageResource = (QueuedMessageResource)element;
         QueuedMessage queuedMessage = queuedMessageResource.getQueuedMessage();
 
-        try {
-            queuedMessage.getQueue().remove(queuedMessage.getKey());
-        } catch (Exception e) {
-            String errorMessage = null;
-            if (e.getMessage() == null) {
-                errorMessage = e.toString();
-            } else {
-                errorMessage = e.getMessage();
-            }
-            MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.Delete_Message_Error, errorMessage);
-            return false;
-        }
-
-        return true;
+        return delegate.doDelete(shell, queuedMessage);
     }
 
     @Override
     public Object[] getChildren(Object o) {
-        return new Object[0];
+        return delegate.getChildren();
     }
 
     @Override
     protected IPropertyDescriptor[] internalGetPropertyDescriptors() {
-
-        PropertyDescriptor[] ourPDs = new PropertyDescriptor[9];
-        ourPDs[0] = new PropertyDescriptor(KEY_FROM, Messages.From);
-        ourPDs[0].setDescription(Messages.From);
-        ourPDs[1] = new PropertyDescriptor(KEY_MSGID, Messages.Message_ID);
-        ourPDs[1].setDescription(Messages.Message_ID);
-        ourPDs[2] = new PropertyDescriptor(KEY_SEV, Messages.Severity);
-        ourPDs[2].setDescription(Messages.Severity);
-        ourPDs[3] = new PropertyDescriptor(KEY_TYPE, Messages.Message_type);
-        ourPDs[3].setDescription(Messages.Message_type);
-        ourPDs[4] = new PropertyDescriptor(KEY_DATE, Messages.Date_sent);
-        ourPDs[4].setDescription(Messages.Date_sent);
-        ourPDs[5] = new PropertyDescriptor(KEY_JOB, Messages.From_job);
-        ourPDs[5].setDescription(Messages.From_job);
-        ourPDs[6] = new PropertyDescriptor(KEY_JOBNBR, Messages.From_job_number);
-        ourPDs[6].setDescription(Messages.From_job_number);
-        ourPDs[7] = new PropertyDescriptor(KEY_PGM, Messages.From_program);
-        ourPDs[7].setDescription(Messages.From_program);
-        ourPDs[8] = new PropertyDescriptor(KEY_REPLY_STS, Messages.Reply_status);
-        ourPDs[8].setDescription(Messages.Reply_status);
-
-        return ourPDs;
+        return delegate.internalGetPropertyDescriptors();
     }
 
     @Override
     public Object internalGetPropertyValue(Object propKey) {
 
-        try {
+        IQueuedMessageResource queuedMessage = (IQueuedMessageResource)propertySourceInput;
 
-            QueuedMessageResource queuedMessage = (QueuedMessageResource)propertySourceInput;
-
-            if (propKey.equals(KEY_FROM)) {
-                return queuedMessage.getQueuedMessage().getUser();
-            }
-
-            if (propKey.equals(KEY_MSGID)) {
-                return queuedMessage.getQueuedMessage().getID();
-            }
-
-            if (propKey.equals(KEY_SEV)) {
-                return new Integer(queuedMessage.getQueuedMessage().getSeverity()).toString();
-            }
-
-            if (propKey.equals(KEY_TYPE)) {
-                return QueuedMessageHelper.getMessageTypeAsText(queuedMessage.getQueuedMessage().getType());
-            }
-
-            if (propKey.equals(KEY_DATE)) {
-                return queuedMessage.getQueuedMessage().getDate().getTime().toString();
-            }
-
-            if (propKey.equals(KEY_JOB)) {
-                return queuedMessage.getQueuedMessage().getFromJobName();
-            }
-
-            if (propKey.equals(KEY_JOBNBR)) {
-                return queuedMessage.getQueuedMessage().getFromJobNumber();
-            }
-
-            if (propKey.equals(KEY_PGM)) {
-                return queuedMessage.getQueuedMessage().getFromProgram();
-            }
-
-            if (propKey.equals(KEY_REPLY_STS)) {
-                return queuedMessage.getQueuedMessage().getReplyStatus();
-            }
-
-        } catch (Exception e) {
-        }
-
-        return null;
+        return delegate.internalGetPropertyValue(queuedMessage, propKey);
     }
 
     public String getAbsoluteParentName(Object element) {
-        return "root"; //$NON-NLS-1$
+        return delegate.getAbsoluteParentName();
     }
 
     public String getSubSystemFactoryId(Object element) {
@@ -221,32 +125,27 @@ public class QueuedMessageResourceAdapter extends AbstractSystemViewAdapter impl
     }
 
     public String getRemoteTypeCategory(Object element) {
-        return "queued messages"; //$NON-NLS-1$ 
+        return delegate.getRemoteTypeCategory();
     }
 
     public String getRemoteType(Object element) {
-        return "queued message"; //$NON-NLS-1$
+        return delegate.getRemoteType();
     }
 
     public String getRemoteSubType(Object arg0) {
-        return null;
+        return delegate.getRemoteSubType();
     }
 
     public boolean refreshRemoteObject(Object oldElement, Object newElement) {
-
-        QueuedMessageResource oldQueuedMessage = (QueuedMessageResource)oldElement;
-        QueuedMessageResource newQueuedMessage = (QueuedMessageResource)newElement;
-        newQueuedMessage.setQueuedMessage(oldQueuedMessage.getQueuedMessage());
-
-        return false;
+        return delegate.refreshRemoteObject(oldElement, newElement);
     }
 
     public Object getRemoteParent(Shell arg0, Object arg1) throws Exception {
-        return null;
+        return delegate.getRemoteParent();
     }
 
     public String[] getRemoteParentNamesInUse(Shell shell, Object element) throws Exception {
-        return null;
+        return delegate.getRemoteParentNamesInUse();
     }
 
     /*
