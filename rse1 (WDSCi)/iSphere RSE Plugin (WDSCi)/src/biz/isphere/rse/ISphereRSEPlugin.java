49d9444c553f54a0cb19ca1679b9c3313729bea7
tools400 <thomas.raddatz@tools400.de>
2015-10-20 16:46:26 +0000

    Tagged methods that must not be changed due to CMOne compatibility constraints.

--- rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/ISphereRSEPlugin.java
+++ rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/ISphereRSEPlugin.java
@@ -78,6 +78,7 @@ public class ISphereRSEPlugin extends AbstractUIPlugin {
         super.initializeImageRegistry(reg);
     }
 
+    // Don`t change this method due to CMOne compatibility reasons
     public static ImageDescriptor getImageDescriptor(String name) {
         String iconPath = "icons/";
         try {
