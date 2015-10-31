56902d865ea51e967c7d98d21b95caf3ece7ff5a
tools400 <thomas.raddatz@tools400.de>
2015-10-31 18:04:57 +0000

    Added javadoc.

--- rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/ibmi/contributions/extension/point/XRDiContributions.java
+++ rse1 (WDSCi)/iSphere RSE Plugin (WDSCi)/src/biz/isphere/rse/ibmi/contributions/extension/point/XRDiContributions.java
@@ -17,8 +17,21 @@ import com.ibm.as400.access.AS400;
 import com.ibm.etools.iseries.core.api.ISeriesConnection;
 import com.ibm.etools.systems.core.messages.SystemMessageException;
 
+/**
+ * This class connects to the
+ * <i>biz.isphere.core.ibmi.contributions.extension.point.IIBMiHostContributions</i>
+ * extension point of the <i>iSphere Core Plugin</i>.
+ * 
+ * @author Thomas Raddatz
+ */
 public class XRDiContributions implements IIBMiHostContributions {
 
+    /**
+     * Finds a matching system for a given host name.
+     * 
+     * @param hostName - Name of the a system is searched for
+     * @return AS400
+     */
     public AS400 findSystem(String hostName) {
 
         try {
@@ -35,11 +48,24 @@ public class XRDiContributions implements IIBMiHostContributions {
         return null;
     }
 
+    /**
+     * Returns a system for a given connection name.
+     * 
+     * @parm connectionName - Name of the connection a system is returned for
+     * @return AS400
+     */
     public AS400 getSystem(String connectionName) {
 
         return getSystem(null, connectionName);
     }
 
+    /**
+     * Returns a system for a given profile and connection name.
+     * 
+     * @parm profile - Profile that is searched for the connection
+     * @parm connectionName - Name of the connection a system is returned for
+     * @return AS400
+     */
     public AS400 getSystem(String profile, String connectionName) {
 
         ISeriesConnection connection = getConnection(profile, connectionName);
@@ -54,11 +80,26 @@ public class XRDiContributions implements IIBMiHostContributions {
         }
     }
 
+    /**
+     * Returns a JDBC connection for a given connection name.
+     * 
+     * @param connectionName - Name of the connection, the JDBC connection is
+     *        returned for
+     * @return Connection
+     */
     public Connection getJdbcConnection(String connectionName) {
 
         return getJdbcConnection(null, connectionName);
     }
 
+    /**
+     * Returns a JDBC connection for a given profile and connection name.
+     * 
+     * @parm profile - Profile that is searched for the JDBC connection
+     * @param connectionName - Name of the connection, the JDBC connection is
+     *        returned for
+     * @return Connection
+     */
     public Connection getJdbcConnection(String profile, String connectionName) {
 
         ISeriesConnection connection = getConnection(profile, connectionName);
@@ -73,6 +114,14 @@ public class XRDiContributions implements IIBMiHostContributions {
         }
     }
 
+    /**
+     * Internal method that returns a connection for a given profile and
+     * connection name. The profile might be null.
+     * 
+     * @parm profile - Profile that is searched for the connection
+     * @parm connectionName - Name of the connection a system is returned for
+     * @return ISeriesConnection
+     */
     private ISeriesConnection getConnection(String profile, String connectionName) {
 
         if (profile == null) {
