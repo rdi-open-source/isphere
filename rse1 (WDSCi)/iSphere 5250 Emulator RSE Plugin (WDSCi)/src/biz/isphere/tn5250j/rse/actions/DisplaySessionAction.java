93591c7a5c1f311715f1ba44f980e2dd4205082d
f_hildebrandt <fhildebrandt@taskforce-it.de>
2015-11-19 16:19:19 +0000

    iSphere 5250 Emulator

--- rse1 (WDSCi)/iSphere 5250 Emulator RSE Plugin (WDSCi)/src/biz/isphere/tn5250j/rse/actions/DisplaySessionAction.java
+++ rse1 (WDSCi)/iSphere 5250 Emulator RSE Plugin (WDSCi)/src/biz/isphere/tn5250j/rse/actions/DisplaySessionAction.java
@@ -1,17 +1,10 @@
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2, or (at your option)
-// any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this software; see the file COPYING.  If not, write to
-// the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-// Boston, MA 02111-1307 USA
+/*******************************************************************************
+ * Copyright (c) 2012-2014 iSphere Project Owners
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
 
 package biz.isphere.tn5250j.rse.actions;
 
