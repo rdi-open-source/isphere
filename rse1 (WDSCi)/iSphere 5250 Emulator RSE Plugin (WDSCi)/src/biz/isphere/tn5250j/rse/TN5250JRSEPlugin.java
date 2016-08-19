7994ee938ee209c56dc96fa7a8640e18a6f90c22
tools400 <thomas.raddatz@tools400.de>
2016-08-19 19:49:44 +0000

    Added option to activate all pinned 5250 sessions on startup.

--- rse1 (WDSCi)/iSphere 5250 Emulator RSE Plugin (WDSCi)/src/biz/isphere/tn5250j/rse/TN5250JRSEPlugin.java
+++ rse1 (WDSCi)/iSphere 5250 Emulator RSE Plugin (WDSCi)/src/biz/isphere/tn5250j/rse/TN5250JRSEPlugin.java
@@ -13,13 +13,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.eclipse.core.runtime.IAdapterManager;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.progress.UIJob;
 import org.osgi.framework.BundleContext;
 
+import biz.isphere.core.internal.viewmanager.IViewManager;
+import biz.isphere.rse.ISphereRSEPlugin;
 import biz.isphere.tn5250j.core.TN5250JCorePlugin;
+import biz.isphere.tn5250j.core.preferences.Preferences;
+import biz.isphere.tn5250j.rse.sessionsview.SessionsView;
 
 public class TN5250JRSEPlugin extends AbstractUIPlugin {
 
@@ -51,6 +62,9 @@ public class TN5250JRSEPlugin extends AbstractUIPlugin {
 
         initializePreferenceStoreDefaults();
 
+        if (Preferences.getInstance().isActivateViewsOnStartup()) {
+            activatePinned5250Sessions();
+        }
     }
 
     @Override
@@ -100,6 +114,34 @@ public class TN5250JRSEPlugin extends AbstractUIPlugin {
 
     public static String getRSESessionDirectory(String connection) {
         return getRSEConnectionDirectory() + File.separator + connection;
+    }
+
+    private void activatePinned5250Sessions() {
+
+        UIJob job = new UIJob("Restoring 5250 sessions ...") {
+
+            @Override
+            public IStatus runInUIThread(IProgressMonitor paramIProgressMonitor) {
+
+                IViewReference[] viewReferences = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getViewReferences();
+                for (IViewReference iViewReference : viewReferences) {
+                    if (SessionsView.ID.equals(iViewReference.getId())) {
+                        IWorkbenchPart part = iViewReference.getPart(false);
+                        if (part == null) {
+                            part = iViewReference.getPart(true);
+                        }
+                        if (part instanceof SessionsView) {
+                            SessionsView sessionsView = (SessionsView)part;
+                            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().bringToTop(sessionsView);
+                        }
+                    }
+                }
+
+                return Status.OK_STATUS;
+            }
+        };
+
+        job.schedule();
     }
 
 }
