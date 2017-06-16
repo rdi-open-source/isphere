323aa8f39fabfbecf8cfdd138874a36bd3963e3c
tools400 <thomas.raddatz@tools400.de>
2017-06-16 16:51:56 +0000

    Fixed "org.eclipse.swt.SWTException: Widget is disposed" error in WDSCi, when trying to reopen a previously closed 5250 view. (Copied from 3.0.3.r)

--- rse1 (WDSCi)/iSphere 5250 Emulator RSE Plugin (WDSCi)/src/biz/isphere/tn5250j/rse/sessionsview/SessionsView.java
+++ rse1 (WDSCi)/iSphere 5250 Emulator RSE Plugin (WDSCi)/src/biz/isphere/tn5250j/rse/sessionsview/SessionsView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2016 iSphere Project Owners
+ * Copyright (c) 2012-2017 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,8 +24,6 @@ public class SessionsView extends CoreSessionsView {
 
     public SessionsView() {
         super();
-
-        getViewManager().add(this);
     }
 
     protected IViewManager getViewManager() {
