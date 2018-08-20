372245f12f0f9b05b570cf471cacd9846c4d332b
tools400 <thomas.raddatz@tools400.de>
2018-08-20 13:34:54 +0000

    Added missing copyright notices.

--- rse1 (WDSCi)/iSphere Job Log Explorer (WDSCi)/src/biz/isphere/joblogexplorer/rse/ISphereJobLogExplorerRSEPlugin.java
+++ rse1 (WDSCi)/iSphere Job Log Explorer (WDSCi)/src/biz/isphere/joblogexplorer/rse/ISphereJobLogExplorerRSEPlugin.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2018 iSphere Project Owners
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
 package biz.isphere.joblogexplorer.rse;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
