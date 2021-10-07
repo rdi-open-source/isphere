139eb75e48db3b3a26e9333f3d421d6d6c8ccaac
tools400 <thomas.raddatz@tools400.de>
2021-10-07 13:00:54 +0000

    Moved obsolete project to folder "obsolete".

--- rse1 (WDSCi)_OBSOLETE_PENDING_FOR_DELETION/iSphere 5250 Emulator Base Plugin (WDSCi)/src/installer/bin/tn5250j.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@rem ======================================
-@rem DOS Batch file to invoke the emulator
-@rem ======================================
-
-SET EMUL_PATH=$INSTALL_PATH
-java -jar "%EMUL_PATH%\lib\tn5250j.jar"
-
-@echo on
