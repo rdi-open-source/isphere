80b771024de1efd68c902dc402e20305bbc27285
Thomas Raddatz <thomas.raddatz@tools400.de>
2024-01-23 18:58:40 +0100

    Upload iSphere release update site.

---
+++ docs/help/assets/menu.js
@@ -0,0 +1,74 @@
+// ---------------------------------------------------------- //
+//  http://www.dynamicdrive.com/dynamicindex1/navigate1.htm   //
+// ---------------------------------------------------------- //
+document.write('\<ul id="treemenu" class="treeview">\
+  <li><a href="../html/introduction/introduction.html" target="content" style="text-decoration:none">Introduction</a></li>\
+  <li><a href="../html/upgrade/upgrade_instructions.html" target="content" style="text-decoration:none">Upgrade Instructions</a></li>\
+  <li>Install Instructions\
+  <ul>\
+    <li><a href="../html/install/wdsci7.0/install_wdsci.html" target="content" style="text-decoration:none">Install Plugin for WDSCi 7.0</a></li>\
+    <li><a href="../html/install/rdi8.0/install_rdi.html" target="content" style="text-decoration:none">Install Plugin for RDi 9.5.1.3+</a></li>\
+    <li><a href="../html/install/install_library.html" target="content" style="text-decoration:none">Install iSphere Library</a></li>\
+  </ul>\
+  </li>\
+  <li>Uninstall Instructions\
+  <ul>\
+    <li><a href="../html/uninstall/uninstall_wdsci/uninstall_wdsci.html" target="content" style="text-decoration:none">Uninstall WDSC</a></li>\
+    <li><a href="../html/uninstall/uninstall_rdi.html" target="content" style="text-decoration:none">Uninstall RDi</a></li>\
+    <li><a href="../html/uninstall/uninstall_library.html" target="content" style="text-decoration:none">Uninstall Library</a></li>\
+  </ul>\
+  </li>\
+  <li>Reference</li>\
+  <li><a href="../html/localupdatesite/localupdatesite.html" target="content" style="text-decoration:none">Local Update Site</a></li>\
+  <li><a href="../html/contributions/contributions.html" target="content" style="text-decoration:none">Contribution</a>\
+  <ul>\
+    <li><a href="../html/contributions/setup/setup_rdp_svn.html" target="content" style="text-decoration:none">Setting up RDP and SVN</a></li>\
+    <li><a href="../html/contributions/setup/setup_isphere.html" target="content" style="text-decoration:none">Setting up iSphere</a></li>\
+    <li><a href="../html/contributions/build/build_isphere.html" target="content" style="text-decoration:none">Building iSphere</a></li>\
+    <li><a href="../html/contributions/specialprojects/specialprojects.html" target="content" style="text-decoration:none">Special Projects</a></li>\
+  </ul>\
+  </li>\
+  <li><a href="../html/api/api.html" target="content" style="text-decoration:none">API</a>\
+  <ul>\
+    <li><a href="../html/api/connection-name.html" target="content" style="text-decoration:none">Connection Name</a></li>\
+  </ul>\
+  </li>\
+  <li>Utilities\
+  <ul>\
+    <li><a href="../html/utilities/strpreprc/strpreprc.html" target="content" style="text-decoration:none">Start Pre-Processor (STRPREPRC)</a>\
+    <ul>\
+      <li><a href="../html/utilities/strpreprc/usage.html" target="content" style="text-decoration:none">Usage</a></li>\
+      <li><a href="../html/utilities/strpreprc/header.html" target="content" style="text-decoration:none">Header</a></li>\
+      <li>Example Headers\
+      <ul>\
+        <li><a href="../html/utilities/strpreprc/example_crtrpgmod.html" target="content" style="text-decoration:none">Create RPG Module</a></li>\
+        <li><a href="../html/utilities/strpreprc/example_crtpgm.html" target="content" style="text-decoration:none">Create Program</a></li>\
+        <li><a href="../html/utilities/strpreprc/example_crtsrvpgm.html" target="content" style="text-decoration:none">Create Service Program</a></li>\
+        <li><a href="../html/utilities/strpreprc/example_crtdtaara.html" target="content" style="text-decoration:none">Create Data Area</a></li>\
+        <li><a href="../html/utilities/strpreprc/example_runsqlstm.html" target="content" style="text-decoration:none">Create SQL Table</a></li>\
+        <li><a href="../html/utilities/strpreprc/example_xcrtsqlobj.html" target="content" style="text-decoration:none">Create SQL Table with CMOne</a></li>\
+      </ul>\
+      </li>\
+      <li><a href="../html/utilities/strpreprc/history.html" target="content" style="text-decoration:none">History</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/utilities/whoami/whoami.html" target="content" style="text-decoration:none">Who am I (WHOAMI)</a></li>\
+  </ul>\
+  </li>\
+  <li>Troubleshooting\
+  <ul>\
+    <li><a href="../html/troubleshooting/debugging_server_jobs/debugging_server_jobs.html" target="content" style="text-decoration:none">Debugging Server Jobs</a></li>\
+    <li><a href="../html/troubleshooting/orphaned_member_locks/orphaned_member_locks.html" target="content" style="text-decoration:none">Orphaned Member Locks</a></li>\
+    <li><a href="../html/troubleshooting/rse_object_decoration/rse_object_decoration.html" target="content" style="text-decoration:none">RSE Object Decoration is Missing</a></li>\
+    <li><a href="../html/troubleshooting/ssl_connections/ssl_connections.html" target="content" style="text-decoration:none">SSL Connection Problems</a></li>\
+    <li><a href="../html/troubleshooting/mysterious_problems/mysterious_problems.html" target="content" style="text-decoration:none">Mysterious Problems</a></li>\
+  </ul>\
+  </li>\
+  <li>Appendix\
+  <ul>\
+    <li><a href="../html/appendix/upload_save_file.html" target="content" style="text-decoration:none">Upload Save File .cmd</a></li>\
+  </ul>\
+  </li>\
+  <li><a href="../html/links/links.html" target="content" style="text-decoration:none">Links</a></li>\
+</ul>\
+');
