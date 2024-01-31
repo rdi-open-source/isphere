d68b83326dd943522acbff76c833433f21f21431
Thomas Raddatz <thomas.raddatz@tools400.de>
2024-01-31 18:53:00 +0100

    Fix iSphere help pages (beta version).

--- docs/beta-version/help/assets/menu.js
+++ docs/beta-version/help/assets/menu.js
@@ -15,7 +15,166 @@ document.write('\<ul id="treemenu" class="treeview">\
     <li><a href="../html/uninstall/uninstall_library.html" target="content" style="text-decoration:none">Uninstall Library</a></li>\
   </ul>\
   </li>\
-  <li>Reference</li>\
+  <li>Reference\
+  <ul>\
+    <li><a href="../html/5250emulator/5250emulator.html" target="content" style="text-decoration:none">5250 Emulator</a>\
+    <ul>\
+      <li><a href="../html/5250emulator/configuration/configuration.html" target="content" style="text-decoration:none">Configuration</a></li>\
+      <li>Sessions\
+      <ul>\
+        <li><a href="../html/5250emulator/session/session.html" target="content" style="text-decoration:none">Session</a></li>\
+        <li><a href="../html/5250emulator/ssl_sessions/ssl_sessions.html" target="content" style="text-decoration:none">SSL Sessions</a></li>\
+        <li><a href="../html/5250emulator/session/designer_session.html" target="content" style="text-decoration:none">Designer Session</a></li>\
+        <li><a href="../html/5250emulator/theme/theme.html" target="content" style="text-decoration:none">Session Theme</a></li>\
+      </ul>\
+      </li>\
+      <li>PDM Options\
+      <ul>\
+        <li><a href="../html/5250emulator/pdm/start_eclipse_editor.html" target="content" style="text-decoration:none">Start Eclipse Editor</a></li>\
+        <li><a href="../html/5250emulator/pdm/start_isphere_compare_merge_editor.html" target="content" style="text-decoration:none">Start iSphere Compare/Merge Editor</a></li>\
+        <li><a href="../html/5250emulator/pdm/set_debugger_sep.html" target="content" style="text-decoration:none">Set Debugger Service Entry Point</a></li>\
+      </ul>\
+      </li>\
+      <li><a href="../html/5250emulator/problems/problems.html" target="content" style="text-decoration:none">Known Problems</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/appearance/appearance.html" target="content" style="text-decoration:none">Appearance</a></li>\
+    <li><a href="../html/bindingdirectoryeditor/bindingdirectoryeditor.html" target="content" style="text-decoration:none">Binding Directory Editor</a></li>\
+    <li><a href="../html/connection/properties.html" target="content" style="text-decoration:none">Connection Properties</a></li>\
+    <li><a href="../html/copymembers/copymembers.html" target="content" style="text-decoration:none">Copy Members</a>\
+    <ul>\
+      <li><a href="../html/copymembers/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a>\
+      <ul>\
+        <li><a href="../html/copymembers/preferences/rule_numerical/rule_numerical.html" target="content" style="text-decoration:none">Numerical Rule</a></li>\
+        <li><a href="../html/copymembers/preferences/rule_numerical_extended/rule_numerical_extended.html" target="content" style="text-decoration:none">Numerical Extended Rule</a></li>\
+      </ul>\
+      </li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/dataareaeditor/dataareaeditor.html" target="content" style="text-decoration:none">Data Area Editor</a>\
+    <ul>\
+      <li><a href="../html/dataareaeditor/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/dataqueues/dataqueues.html" target="content" style="text-decoration:none">Data Queues</a>\
+    <ul>\
+      <li><a href="../html/dataqueues/monitor/dataqueuemonitor.html" target="content" style="text-decoration:none">Monitor</a></li>\
+      <li><a href="../html/dataqueues/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/dataspaces/dataspaces.html" target="content" style="text-decoration:none">Data Spaces</a>\
+    <ul>\
+      <li><a href="../html/dataspaces/designer/dataspaceeditordesigner.html" target="content" style="text-decoration:none">Designer</a></li>\
+      <li><a href="../html/dataspaces/editor/dataspaceeditor.html" target="content" style="text-decoration:none">Editor</a></li>\
+      <li><a href="../html/dataspaces/monitor/dataspacemonitor.html" target="content" style="text-decoration:none">Monitor</a></li>\
+      <li><a href="../html/dataspaces/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/debuggeradditions/debuggeradditions.html" target="content" style="text-decoration:none">Debugger Additions</a></li>\
+    <li><a href="../html/displaymoduleview/displaymoduleview.html" target="content" style="text-decoration:none">Display Module View</a></li>\
+    <li><a href="../html/core/core.html" target="content" style="text-decoration:none">iSphere Core</a></li>\
+    <li><a href="../html/decorator/hostobjectdecorator.html" target="content" style="text-decoration:none">Host Object Decorator</a>\
+    <ul>\
+      <li><a href="../html/decorator/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/messagefilecompareeditor/messagefilecompareeditor.html" target="content" style="text-decoration:none">Message File Compare Editor</a>\
+    <ul>\
+      <li><a href="../html/messagefilecompareeditor/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/messagefileeditor/messagefileeditor.html" target="content" style="text-decoration:none">Message File Editor</a></li>\
+    <li><a href="../html/messagefilesearch/messagefilesearch.html" target="content" style="text-decoration:none">Message File Search</a>\
+    <ul>\
+      <li><a href="../html/messagefilesearch/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/retrievebindersource/retrievebindersource.html" target="content" style="text-decoration:none">Retrieve Binder Source</a></li>\
+    <li>RSE Resource Management\
+    <ul>\
+      <li><a href="../html/resourcemanagement/filtermanagement/filtermanagement.html" target="content" style="text-decoration:none">RSE Filter Management</a></li>\
+      <li><a href="../html/resourcemanagement/commandmanagement/commandmanagement.html" target="content" style="text-decoration:none">RSE Command Management</a></li>\
+      <li><a href="../html/resourcemanagement/useractionmanagement/useractionmanagement.html" target="content" style="text-decoration:none">RSE User Action Management</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/sourcefilecomparemergeeditor/sourcefilecomparemergeeditor.html" target="content" style="text-decoration:none">Source File Compare/Merge Editor</a>\
+    <ul>\
+      <li><a href="../html/sourcefilecomparemergeeditor/rdi95extensions/rdi95extensions.html" target="content" style="text-decoration:none">RDi 9.5 Extensions</a></li>\
+      <li><a href="../html/sourcefilecomparemergeeditor/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/sourcefilesearch/sourcefilesearch.html" target="content" style="text-decoration:none">Source File Search</a>\
+    <ul>\
+      <li><a href="../html/search/regularexpressions/regularexpressions.html" target="content" style="text-decoration:none">Regular Expressions</a></li>\
+      <li><a href="../html/sourcefilesearch/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/streamfilecomparemergeeditor/streamfilecomparemergeeditor.html" target="content" style="text-decoration:none">Stream File Compare/Merge Editor</a>\
+    <ul>\
+      <li><a href="../html/streamfilecomparemergeeditor/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/streamfilesearch/streamfilesearch.html" target="content" style="text-decoration:none">Stream File Search</a>\
+    <ul>\
+      <li><a href="../html/search/regularexpressions/regularexpressions.html" target="content" style="text-decoration:none">Regular Expressions</a></li>\
+      <li><a href="../html/streamfilesearch/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/spooledfilesubsystem/spooledfilesubsystem.html" target="content" style="text-decoration:none">Spooled File Subsystem</a>\
+    <ul>\
+      <li><a href="../html/spooledfilesubsystem/workwithspooledfiles/workwithspooledfiles.html" target="content" style="text-decoration:none">Work With Spooled Files</a></li>\
+      <li><a href="../html/spooledfilesubsystem/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/userspaceeditor/userspaceeditor.html" target="content" style="text-decoration:none">User Space Editor</a>\
+    <ul>\
+      <li><a href="../html/dataareaeditor/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/joblogexplorer/joblogexplorer.html" target="content" style="text-decoration:none">Job Log Explorer</a>\
+    <ul>\
+      <li><a href="../html/sql/sql_reference.html" target="content" style="text-decoration:none">SQL Reference</a></li>\
+      <li><a href="../html/joblogexplorer/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+      <li><a href="../html/joblogexplorer/configuration/configuration.html" target="content" style="text-decoration:none">Configuration</a></li>\
+      <li><a href="../html/joblogexplorer/functioning/functioning.html" target="content" style="text-decoration:none">How it Works</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/jobtraceexplorer/jobtraceexplorer.html" target="content" style="text-decoration:none">Job Trace Explorer</a>\
+    <ul>\
+      <li><a href="../html/sql/sql_reference.html" target="content" style="text-decoration:none">SQL Reference</a></li>\
+      <li><a href="../html/jobtraceexplorer/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/journalexplorer/journalexplorer.html" target="content" style="text-decoration:none">Journal Explorer</a>\
+    <ul>\
+      <li><a href="../html/sql/sql_reference.html" target="content" style="text-decoration:none">SQL Reference</a></li>\
+      <li><a href="../html/journalexplorer/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li>LPEX Editor Extensions\
+    <ul>\
+      <li><a href="../html/comments/comments.html" target="content" style="text-decoration:none">Comments</a></li>\
+      <li><a href="../html/comments/indentions.html" target="content" style="text-decoration:none">Indentions</a></li>\
+      <li><a href="../html/comments/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+      <li>Tips and Tricks\
+      <ul>\
+        <li><a href="../html/comments/tips_and_tricks/remapping_enter_key.html" target="content" style="text-decoration:none">Re-Mapping Enter Key</a></li>\
+      </ul>\
+      </li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/messagesubsystem/messagesubsystem.html" target="content" style="text-decoration:none">Message Subsystem</a>\
+    <ul>\
+      <li><a href="../html/messagesubsystem/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/tasktags/tasktags.html" target="content" style="text-decoration:none">LPEX Task Tags</a>\
+    <ul>\
+      <li><a href="../html/tasktags/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+  </ul>\
+  </li>\
   <li><a href="../html/localupdatesite/localupdatesite.html" target="content" style="text-decoration:none">Local Update Site</a></li>\
   <li><a href="../html/contributions/contributions.html" target="content" style="text-decoration:none">Contribution</a>\
   <ul>\
@@ -27,6 +186,10 @@ document.write('\<ul id="treemenu" class="treeview">\
   <li><a href="../html/api/api.html" target="content" style="text-decoration:none">API</a>\
   <ul>\
     <li><a href="../html/api/connection-name.html" target="content" style="text-decoration:none">Connection Name</a></li>\
+    <li><a href="../html/api/core-plugin-api.html" target="content" style="text-decoration:none">Core Plug-in API</a></li>\
+    <li><a href="../html/api/job-log-explorer-plugin-api.html" target="content" style="text-decoration:none">Job Log Explorer Plug-in API</a></li>\
+    <li><a href="../html/api/job-trace-explorer-plugin-api.html" target="content" style="text-decoration:none">Job Trace Explorer Plug-in API</a></li>\
+    <li><a href="../html/api/journal-explorer-plugin-api.html" target="content" style="text-decoration:none">Journal Explorer Plug-in API</a></li>\
   </ul>\
   </li>\
   <li>Utilities\
@@ -46,6 +209,11 @@ document.write('\<ul id="treemenu" class="treeview">\
       </ul>\
       </li>\
       <li><a href="../html/utilities/strpreprc/history.html" target="content" style="text-decoration:none">History</a></li>\
+      <li><a href="../html/strpreprc/strpreprc.html" target="content" style="text-decoration:none">Plug-in</a>\
+      <ul>\
+        <li><a href="../html/strpreprc/preferences/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+      </ul>\
+      </li>\
     </ul>\
     </li>\
     <li><a href="../html/utilities/whoami/whoami.html" target="content" style="text-decoration:none">Who am I (WHOAMI)</a></li>\
